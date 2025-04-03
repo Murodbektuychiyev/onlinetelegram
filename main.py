dd8e78847bfb48d32cad69b596dda81827d4471f
Murodbek Tuychiyev <murodbektuychiyev83@gmail.com>
2025-04-03 19:57:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+import os
 from telethon.sync import TelegramClient
 import asyncio
 
-# API ID va API HASH ma'lumotlarini o'zingizga moslab qo'ying
-api_id = 24410919  # O'zingizning API ID-ni yozing
-api_hash = '11a3a20ee4f9e851e35412b0a2eedb3a'  # O'zingizning API HASH-ni yozing
-phone_number = '+998500175636'  # O'zingizning telefon raqamingizni yozing
-
-# Session nomini to'g'ri kiriting (faylni yuklagan nom)
-session_name = 'session_name'  # Faylingiz nomini kiriting
+# Muhit o‘zgaruvchilaridan API ID, API HASH va telefon raqamini olish
+api_id = int(os.getenv("API_ID", 24410919))
+api_hash = os.getenv("API_HASH", "11a3a20ee4f9e851e35412b0a2eedb3a")
+phone_number = os.getenv("PHONE_NUMBER", "+998945975009")
+session_name = os.getenv("SESSION_NAME", "session_name")  # Fayl nomi
 
 # TelegramClient yaratish
 client = TelegramClient(session_name, api_id, api_hash)
@@ -15,12 +14,10 @@ client = TelegramClient(session_name, api_id, api_hash)
 async def main():
     await client.start(phone_number)
     print("✅ Session faylidan foydalanish boshlandi.")
-    
-    # Telegram akkauntini doimiy onlayn holatda ushlab turish
+
     while True:
-        await client.send_message('me', 'Online')  # Har 60 sekundda o'zingizga xabar yuboriladi
+        await client.send_message('me', 'Online')
         await asyncio.sleep(60)  # 60 sekund kutadi
 
 if __name__ == "__main__":
-    import asyncio
     client.loop.run_until_complete(main())
